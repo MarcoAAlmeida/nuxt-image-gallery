90620c3dfdee1836c9ce91166ee9c9475431726c
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2024-11-03 22:31:04 -0300

    extended theme to add a new color style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,13 @@ import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
   theme: {
-    colors: {
-      vintage: '#f9d8e2',
-      ...defaultTheme.colors
-    },
     fontFamily: {
       sans: ['Inter var', 'Inter', ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      colors: {
+        vintage: '#f9d8e2'
+      }
     }
   }
 }
